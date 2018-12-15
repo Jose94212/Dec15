5dc39fa4d3b2f85865a0800da7d477b5ca904c35
Jose94212 <jose.g.payyappilly@gmail.com>
2018-12-16 00:51:29 +0530

    2ndCommit with selenium jars

--- Dec15MavenTry/src/testClass.java
+++ Dec15MavenTry/src/testClass.java
@@ -1,8 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class testClass {
 	@Test
 	public void testmethod() {
 		System.out.println("hey i am in TESTNg met");
+	}
+	@Test
+	public void testmethod2() {
+		System.out.println("in testmethod 2");
+	}
+	@Test
+	public void testSeleniumDriver() {
+		System.setProperty("webdriver.gecko.driver", "/home/jose/Downloads/Selenium/geckodriver-v0.21.0-linux64/geckodriver");
+		WebDriver driver= new FirefoxDriver();
+		driver.close();
 	}
 }
